0ef6f68ab632efb7d27aa52f72334c0227437d2c
YunEunMi <5272yun@naver.com>
2024-05-22 22:35:40 +0900

    Update controllers

--- controllers/example_controllers.py
+++ controllers/example_controllers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from server.services.example_services import login_user, logout_user
+from services.example_services import login_user, logout_user, fetch_workers_by_boss, get_user_profile,add_worker, delete_alba, get_schedule
 
 bp = Blueprint('auth', __name__)
 
@@ -12,3 +12,26 @@ def login():
 @bp.route('/logout')
 def logout():
     return logout_user()
+
+
+@bp.route('/workers', methods=['GET'])
+def get_workers():
+    bossno = request.args.get('bossno')
+    return fetch_workers_by_boss(bossno)
+
+@bp.route('/profile', methods=['GET'])
+def profile():
+    return get_user_profile(request)
+
+@bp.route('/create', methods=['POST'])
+def create():
+    return add_worker()
+
+@bp.route('/delete/<int:albano>', methods=['DELETE'])
+def delete(albano):
+    return delete_alba(albano)
+
+
+@bp.route('/schedule/<int:bossno>', methods=['GET'])
+def schedule(bossno):
+    return get_schedule(bossno)
