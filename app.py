70841c15b2f26595eaf116b55e94d0f81a143203
YunEunMi <5272yun@naver.com>
2024-05-22 22:37:00 +0900

    Add app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from server.models.example_models import db
-from server.controllers.example_controllers import bp as auth_bp
+from models.example_models import db
+from controllers.example_controllers import bp as auth_bp
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/albatime_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+
     app.secret_key = "123"
 
     db.init_app(app)
@@ -16,5 +18,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    db.create_all(app=app)
-    app.run(debug=True)
+    db.create_all(app)
+    app.run(host='0.0.0.0',port="5000",debug=False)
